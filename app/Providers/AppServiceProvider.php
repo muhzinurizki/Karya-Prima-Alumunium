35a3cf4c9d88477cf31d5ebf8c9d4eb3b0a8a3b9
Muhzinur Rizki <139126479+muhzinurizki@users.noreply.github.com>
2026-01-09 12:06:02 +0700

    Setup Vercel deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Tambahkan Baris Ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Gunakan \URL atau URL saja sekarang sudah aman
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
